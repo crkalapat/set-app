5bc1e55e00dda7e8940b219b8995dfa0114ef0ee
Charlie Kalapati <charlie.r.kalapati@gmail.com>
2025-10-17 11:34:08 -0500

    Initial Commit

---
+++ Set/SetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetApp.swift
+//  Set
+//
+//  Created by Charlie Kalapati on 10/17/25.
+//
+
+import SwiftUI
+
+@main
+struct SetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
